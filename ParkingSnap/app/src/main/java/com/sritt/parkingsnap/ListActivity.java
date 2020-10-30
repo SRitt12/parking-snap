3c1ba90d3015cd6c1d7b75b24488de10c2cf5c24
LundukS <59974861+LundukS@users.noreply.github.com>
2020-10-30 14:16:13 -0500

    Update ListActivity.java
    add functionality to the new vehicle layout

--- ParkingSnap/app/src/main/java/com/sritt/parkingsnap/ListActivity.java
+++ ParkingSnap/app/src/main/java/com/sritt/parkingsnap/ListActivity.java
@@ -4,14 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.io.Serializable;
 import java.util.HashMap;
 
-public class ListActivity extends AppCompatActivity {
+public class ListActivity extends AppCompatActivity implements Serializable {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
     }
-    HashMap<String, Vehicle> vehicles = new HashMap();
+     static HashMap<String, Vehicle> vehicles = new HashMap();
+
 }
