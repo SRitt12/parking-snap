803d462b4854c89310c1cd83a0137fda44438762
LundukS <59974861+LundukS@users.noreply.github.com>
2020-10-30 14:18:26 -0500

    Update Vehicle.java
    add atributes to vehicle

--- ParkingSnap/app/src/main/java/com/sritt/parkingsnap/Vehicle.java
+++ ParkingSnap/app/src/main/java/com/sritt/parkingsnap/Vehicle.java
@@ -1,17 +1,21 @@
 package com.sritt.parkingsnap;
 
 public class Vehicle {
+    String firstName;
+    String lastName;
     String licence;
     String year;
     String make;
     String model;
     String color;
 
-    public Vehicle(String licence, String year, String make, String model, String color) {
+    public Vehicle(String firstName, String lastName, String licence, String year, String make, String model, String color) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.licence = licence;
         this.year = year;
         this.make = make;
-        this.model= model;
+        this.model = model;
         this.color = color;
     }
 }
