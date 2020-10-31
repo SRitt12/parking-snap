f5c4b9341db017bd30e29cb20cccd224fda11fc6
LundukS <59974861+LundukS@users.noreply.github.com>
2020-10-30 20:35:52 -0500

    Update Vehicle.java
    add getters and setters and some attributes

--- ParkingSnap/app/src/main/java/com/sritt/parkingsnap/Vehicle.java
+++ ParkingSnap/app/src/main/java/com/sritt/parkingsnap/Vehicle.java
@@ -1,6 +1,8 @@
 package com.sritt.parkingsnap;
 
-public class Vehicle {
+import java.io.Serializable;
+
+public class Vehicle implements Serializable {
     String firstName;
     String lastName;
     String licence;
@@ -8,6 +10,62 @@ public class Vehicle {
     String make;
     String model;
     String color;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLicence() {
+        return licence;
+    }
+
+    public void setLicence(String licence) {
+        this.licence = licence;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
     public Vehicle(String firstName, String lastName, String licence, String year, String make, String model, String color) {
         this.firstName = firstName;
@@ -17,5 +75,9 @@ public class Vehicle {
         this.make = make;
         this.model = model;
         this.color = color;
+    }
+
+    public String toString(){
+        return("Name: "+ firstName+" "+lastName+"\nLicense Plate Number: "+licence+"\nVehicle: "+make+" "+model+" "+year+"\nColor: "+color);
     }
 }
